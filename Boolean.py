e4f87561bbdc435e1c2359953c1584ba5a4a57f5
HtetKo510217 <htetko510217@gmail.com>
2023-04-21 14:37:23 +0630

    Boolean , Comparison and Logical Operator

---
+++ Boolean.py
@@ -0,0 +1,12 @@
+# Comparison operator
+print(5 > 3)
+print(5 > 5)
+print(5 >= 5)
+print(5 <= 5)
+
+# logical operator (and or not)
+
+print(not True)
+print(not False)
+print(5 > 2 and 5 >= 5)
+print(5 > 2 or 5 > 15)
