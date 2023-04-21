58693d079318e34ae76f3bd74c12fe7506a3f2d5
HtetKo510217 <htetko510217@gmail.com>
2023-04-21 16:06:18 +0630

    range use in loop

---
+++ range.py
@@ -0,0 +1,10 @@
+# range(a=default(0),b,c(optional)) => c is jump step
+
+# for num in range(5):
+#     print(f'hello word')
+
+
+users = ['mg mg', 'aung aung', 'su su', 'htet ko']
+
+for num in range(len(users)):
+    print(users[num])
