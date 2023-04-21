a95a5cc1c453bc0c36d649d20c26846cdac77e1a
HtetKo510217 <htetko510217@gmail.com>
2023-04-21 21:16:50 +0630

    dictonaries in python

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+person = {
+    'name': 'Htet Ko',
+    'age': 28
+}
+
+person['hair-color'] = 'black'
+
+# print(person)
+print('name' in person)
+
+person_keys = list(person.keys())
+print(person_keys)
+
+person_values = list(person.values())
+print(person_values)
