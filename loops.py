7c844d40d980cfcf07bbd44f16fc931f1dd89470
HtetKo510217 <htetko510217@gmail.com>
2023-04-21 15:59:08 +0630

    Loops in python

---
+++ loops.py
@@ -0,0 +1,19 @@
+# for in loop
+# names = ['mg mg', 'aung aung', 'htet ko', 'su su']
+
+# for name in names:
+#     if name == 'htet ko':
+#         print(f'{name} is admin')
+#         break
+#     else:
+#         print(f'{name} is normal user')
+
+
+# while loop
+num = 0
+while num < 10:
+    if num > 5:
+        break
+    if (num % 2 == 0):
+        print(f'{num}')
+    num += 1
